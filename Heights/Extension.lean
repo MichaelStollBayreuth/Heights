1d9fe674d9d038fd24451b1006d1690d8f4d0834
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2025-01-14 12:02:05 +0100

    move material on restrict to Extension

--- Heights/Extension.lean
+++ Heights/Extension.lean
@@ -1,7 +1,7 @@
 import Heights.Equivalence
 
 /-!
-# Estensions of absolute values
+# Extensions of absolute values
 
 Let `F` be a field with an absolute value `v` on it, and let `F'` be a field extension of `F`
 of finite degree.
@@ -13,6 +13,131 @@ where `localDegree F v'` is the degree of the extension of completions `F'_v' / 
 We follow [Brian Conrad's notes}(http://math.stanford.edu/~conrad/676Page/handouts/ostrowski.pdf),
 Sections 6 and 7.
 -/
+
+/-!
+### Auxiliary lemmas
+-/
+
+section restrict
+
+namespace AbsoluteValue
+
+variable {F R S : Type*} [Field F] [Semiring R] [Nontrivial R] [Algebra F R] [OrderedSemiring S]
+
+variable (F) in
+/-- The restriction to a field `F` of an absolute value on an `F`-algebra `R`. -/
+def restrict (v : AbsoluteValue R S) : AbsoluteValue F S :=
+  v.comp (RingHom.injective (algebraMap F R))
+
+@[simp]
+lemma apply_algebraMap (v : AbsoluteValue R S) (x : F) :
+    v (algebraMap F R x) = v.restrict F x := rfl
+
+lemma isNontrivial_of_restrict (v : AbsoluteValue R S) (h : (v.restrict F).IsNontrivial) :
+    v.IsNontrivial := by
+  obtain ⟨x, hx₀, hx₁⟩ := h
+  exact ⟨algebraMap F R x, (map_ne_zero _).mpr hx₀, hx₁⟩
+
+/-- Two equivalent extensions from `F` to `R` of the same nontrivial absolute value
+must be equal. -/
+lemma eq_of_equivalent_and_restrict_eq {v₁ v₂ : AbsoluteValue R ℝ} (h₁ : v₁ ≈ v₂)
+    (h₂ : v₁.restrict F = v₂.restrict F) (h₃ : (v₁.restrict F).IsNontrivial) :
+    v₁ = v₂ := by
+  obtain ⟨c, hc₀, hc₁⟩ := h₁
+  obtain ⟨x, hx⟩ := h₃.exists_abv_gt_one
+  suffices c = 1 by simpa [this] using hc₁
+  have H : v₁ (algebraMap F R x) = v₂ (algebraMap F R x) := by
+    simp only [apply_algebraMap, h₂]
+  rw [← congrFun hc₁, apply_algebraMap, eq_comm] at H
+  nth_rewrite 2 [← Real.rpow_one (v₁.restrict F x)] at H
+  exact (Real.rpow_right_inj (zero_lt_one.trans hx) hx.ne').mp H
+
+/-- The extension of the trivial absolute value on a field `F` to a finite-dimensional `F`-algebra
+that is a division ring is trivial. -/
+lemma trivial_of_finiteDimensional_of_restrict {R : Type*} [DivisionRing R] [Nontrivial R]
+    [Algebra F R] [FiniteDimensional F R] {v : AbsoluteValue F ℝ} {v' : AbsoluteValue R ℝ}
+    (h : v'.restrict F = v) (hv : ¬ v.IsNontrivial) :
+    ¬ v'.IsNontrivial := by
+  rw [not_isNontrivial_iff] at hv ⊢
+  suffices key : ∀ {x} (hx : x ≠ 0), v' x ≤ 1 by
+    intro x hx
+    refine le_antisymm (key hx) ?_
+    rw [← inv_inv x, map_inv₀, one_le_inv₀ <| v'.pos <| inv_ne_zero hx]
+    exact key <| inv_ne_zero hx
+  intro x hx₀
+  by_contra! hx
+  let n := Module.finrank F R
+  obtain ⟨m, hm⟩ : ∃ m : ℕ, n < v' (x ^ m) := by
+    simpa only [v'.map_pow] using pow_unbounded_of_one_lt _ hx
+  contrapose! hm; clear hm
+  have hxm : x ^ m ≠ 0 := pow_ne_zero m hx₀
+  -- write `x ^ m` as a linear combination of `≤ n` terms `c • (x ^ m) ^ (-i)` with `i ≥ 0`
+  let p := (LinearMap.mulLeft F (x ^ m)).charpoly
+  have hp₁ : p.Monic := LinearMap.charpoly_monic _
+  have hpn : p.natDegree = n := LinearMap.charpoly_natDegree _
+  have H : p.aeval (x ^ m) = 0 := Algebra.charpoly_aeval_self_eq_zero _
+  rw [← p.eraseLead_add_C_mul_X_pow, Polynomial.aeval_add, Polynomial.aeval_eq_sum_range,
+    add_eq_zero_iff_eq_neg', hp₁.leadingCoeff, map_one, one_mul, map_pow, Polynomial.aeval_X] at H
+  apply_fun (· * (x ^ m) ^ (-n + 1 : ℤ)) at H
+  rw [hpn, ← zpow_natCast, ← zpow_add' (.inl hxm), add_neg_cancel_left, zpow_one, neg_mul,
+    Finset.sum_mul] at H
+  conv at H => enter [2, 1, 2, i]; rw [smul_mul_assoc, ← zpow_natCast, ← zpow_add' (.inl hxm)]
+  rw [H, v'.map_neg]; clear H
+  have pel : p.eraseLead.natDegree + 1 ≤ n := LinearMap.charpoly_eraseLead_natDegree_le _
+  have pel' : (p.eraseLead.natDegree + 1 : ℝ) ≤ n := mod_cast pel
+  have help (N : ℕ) : (N + 1 : ℝ) = ∑ i ∈ Finset.range (N + 1), 1 := by simp
+  refine (v'.sum_le ..).trans (LE.le.trans ?_ pel')
+  conv_rhs => rw [help]
+  refine Finset.sum_le_sum fun i hi ↦ ?_
+  rw [Algebra.smul_def, v'.map_mul, apply_algebraMap, h]
+  rcases eq_or_ne (p.eraseLead.coeff i) 0 with H | H
+  · simp [H]
+  · simp only [ne_eq, H, not_false_eq_true, hv, map_zpow₀, v'.map_pow, one_mul]
+    simp only [Finset.mem_range] at hi
+    exact zpow_le_one_of_nonpos₀ (one_le_pow₀ hx.le) <| by omega
+
+variable {F' : Type*} [Ring F'] [Algebra F F'] {v : AbsoluteValue F ℝ}
+
+@[simp]
+lemma equiv_symm_apply_algebraMap (v' : AbsoluteValue F' ℝ) (x : WithAbs v) :
+    (WithAbs.equiv v').symm (algebraMap F F' (WithAbs.equiv v x)) =
+      algebraMap (WithAbs v) (WithAbs v') x := by
+  rw [← WithAbs.equiv_apply_algebraMap v', Equiv.symm_apply_apply]
+
+variable [Nontrivial F']
+
+open WithAbs
+
+@[simp]
+lemma apply_algebraMap_withAbs {v' : AbsoluteValue F' ℝ} (h : v'.restrict F = v) (x : WithAbs v) :
+    v' (WithAbs.equiv v' (algebraMap (WithAbs v) (WithAbs v') x)) = v (WithAbs.equiv v x) := by
+  rw [WithAbs.equiv_apply_algebraMap, apply_algebraMap, h]
+
+@[fun_prop]
+lemma continuous_algebraMap {v' : AbsoluteValue F' ℝ} (h : v'.restrict F = v) :
+    Continuous <| algebraMap (WithAbs v) (WithAbs v') := by
+  rw [continuous_iff_continuous_dist]
+  conv => enter [1, x]; simp only [← equiv_symm_apply_algebraMap v']
+  simp_rw [dist_eq_norm_sub, norm_eq_abv, WithAbs.equiv_sub, Equiv.apply_symm_apply, ← map_sub,
+    apply_algebraMap, h, ← WithAbs.equiv_sub, ← norm_eq_abv, ← dist_eq_norm_sub]
+  exact continuous_dist
+
+instance continuousSMul {v' : AbsoluteValue F' ℝ} [Fact <| v'.restrict F = v] :
+    ContinuousSMul (WithAbs v) (WithAbs v') where
+  continuous_smul := (continuous_algebraMap_iff_smul _ _).mp <| continuous_algebraMap Fact.out
+
+lemma isNonarchmedean_restrict_iff {F' : Type*} [Field F'] [Algebra F F']
+    {v : AbsoluteValue F' ℝ} :
+    IsNonarchimedean (v.restrict F) ↔ IsNonarchimedean v := by
+  have H (n : ℕ) : v.restrict F n = v n := by
+    rw [← apply_algebraMap]
+    congr
+    exact algebraMap.coe_natCast n
+  simp_rw [isNonarchimedean_iff_le_one_on_nat, H]
+
+end AbsoluteValue
+
+end restrict
 
 section complete
 
