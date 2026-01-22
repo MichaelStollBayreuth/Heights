652d3ad2ef7e38b58e2754b819c864e2268f8629
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-01-22 19:54:01 +0100

    bump Mathlib

--- Heights/Instances.lean
+++ Heights/Instances.lean
@@ -27,12 +27,4 @@ variable {K : Type*} [Field K]
 
 variable  [NumberField K]
 
-/-- This is the familiar definition of the multiplicative height on tuples
-of number field elements. -/
-lemma mulHeight_eq {ι : Type*} {x : ι → K} (hx : x ≠ 0) :
-    mulHeight x =
-      (∏ v : InfinitePlace K, (⨆ i, v (x i)) ^ v.mult) * ∏ᶠ v : FinitePlace K, ⨆ i, v (x i) := by
-  simp only [FinitePlace.coe_apply, InfinitePlace.coe_apply, Height.mulHeight_eq hx,
-    prod_archAbsVal_eq, prod_nonarchAbsVal_eq fun v ↦ ⨆ i, v (x i)]
-
 end NumberField
