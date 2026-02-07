a1730540b9415441e033fdbb18937ad924703670
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-02-07 17:33:30 +0100

    minor simplification in NumberField.lean

--- Heights/NumberField.lean
+++ Heights/NumberField.lean
@@ -21,7 +21,7 @@ We provide instances of `Height.AdmissibleAbsValues` for
 
 namespace NumberField
 
-open Height
+open Height Finset Multiset
 
 variable {K : Type*} [Field K]
 
@@ -32,13 +32,12 @@ open AdmissibleAbsValues
 lemma sum_archAbsVal_eq {M : Type*} [AddCommMonoid M] (f : AbsoluteValue K ℝ → M) :
     (archAbsVal.map f).sum = ∑ v : InfinitePlace K, v.mult • f v.val := by
   classical
-  rw [Finset.sum_multiset_map_count]
-  exact Finset.sum_bij' (fun w hw ↦ ⟨w, mem_multisetInfinitePlace.mp <| Multiset.mem_dedup.mp hw⟩)
-    (fun v _ ↦ v.val) (fun _ _ ↦ Finset.mem_univ _)
-    (fun v _ ↦ by simp [v.isInfinitePlace, archAbsVal])
+  rw [sum_multiset_map_count]
+  exact sum_bij' (fun w hw ↦ ⟨w, mem_multisetInfinitePlace.mp <| mem_dedup.mp hw⟩)
+    (fun v _ ↦ v.val) (fun _ _ ↦ mem_univ _) (fun v _ ↦ by simp [v.isInfinitePlace, archAbsVal])
     (fun _ _ ↦ rfl) (fun _ _ ↦ rfl)
     fun w hw ↦ by
-      simp only [archAbsVal, Multiset.mem_toFinset, mem_multisetInfinitePlace] at hw ⊢
+      simp only [archAbsVal, mem_toFinset, mem_multisetInfinitePlace] at hw ⊢
       simp [count_multisetInfinitePlace_eq_mult ⟨w, hw⟩]
 
 lemma sum_nonarchAbsVal_eq {M : Type*} [AddCommMonoid M] (f : AbsoluteValue K ℝ → M) :
