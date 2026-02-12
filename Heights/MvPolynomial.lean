b1d29b7e707758b68b7c92aa35b26f66b3fdb513
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-02-12 20:34:15 +0100

    add linearMap_apply_le

--- Heights/MvPolynomial.lean
+++ Heights/MvPolynomial.lean
@@ -165,6 +165,27 @@ lemma ciSup_sup [Nonempty ι] {f : ι → α} {a : α} :
 
 end Finite
 
+namespace IsNonarchimedean
+
+variable {R α : Type*} [CommRing R]
+
+/- The ultrametric triangle inequality for finite sums. -/
+private lemma apply_sum_le {v : AbsoluteValue R ℝ} (hv : IsNonarchimedean v) {l : α → R} {s : Finset α} :
+    v (∑ i ∈ s, l i) ≤ ⨆ i : s, v (l i) := by
+  classical
+  induction s using Finset.induction with
+  | empty => simp
+  | insert a s ha ih =>
+    rw [Finset.sum_insert ha]
+    grw [hv .., ih]
+    refine max_le ?_ ?_
+    · exact Finite.le_ciSup_of_le ⟨_, s.mem_insert_self a⟩ le_rfl
+    · rcases isEmpty_or_nonempty s with hs | hs
+      · simpa using v.iSup_abv_nonneg
+      exact ciSup_le fun i ↦ Finite.le_ciSup_of_le (⟨i.val, Finset.mem_insert_of_mem i.prop⟩) le_rfl
+
+end IsNonarchimedean
+
 end aux
 
 namespace Height
@@ -418,3 +439,91 @@ lemma logHeight₁_eval_le (p : K[X]) (x : K) :
   exact (log_le_log <| by positivity) <| mulHeight₁_eval_le p x
 
 end Polynomial
+
+/-!
+### Upper bound for the height of the image under a linear map
+-/
+
+namespace Height
+
+variable {K : Type*} [Field K] {ι ι' : Type*} [Fintype ι] [Finite ι']
+
+-- The "local" version of the bound for (archimedean) absolute values.
+lemma linearMap_apply_bound [Nonempty ι'] (v : AbsoluteValue K ℝ) (A : ι' × ι → K) (x : ι → K) :
+    ⨆ j, v (∑ i, A (j, i) * x i) ≤ Nat.card ι * (⨆ ji, v (A ji)) * ⨆ i, v (x i) := by
+  refine ciSup_le fun j ↦ ?_
+  grw [v.sum_le]
+  simp only [map_mul]
+  grw [Finset.sum_le_sum (g := fun _ ↦ (⨆ ji, v (A ji)) * ⨆ i, v (x i)) fun i _ ↦ ?h]
+  case h =>
+    simp only
+    gcongr
+    · exact v.iSup_abv_nonneg
+    · exact Finite.le_ciSup_of_le (j, i) le_rfl
+    · exact Finite.le_ciSup_of_le i le_rfl
+  rw [Finset.sum_const, nsmul_eq_mul, mul_assoc, Finset.card_univ, Nat.card_eq_fintype_card]
+
+-- The "local" version of the bound for nonarchimedean absolute values.
+lemma linearMap_apply_bound_of_isNonarchimedean [Nonempty ι] [Nonempty ι'] {v : AbsoluteValue K ℝ}
+    (hv : IsNonarchimedean v) (A : ι' × ι → K) (x : ι → K) :
+    ⨆ j, v (∑ i, A (j, i) * x i) ≤ (⨆ ji, v (A ji)) * ⨆ i, v (x i) := by
+  refine ciSup_le fun j ↦ ?_
+  grw [hv.apply_sum_le]
+  simp only [map_mul]
+  have (f : ι → ℝ) : ⨆ i : ↥Finset.univ, f i.val = ⨆ i, f i :=
+    Function.Surjective.iSup_comp (fun i ↦ ⟨⟨i, Finset.mem_univ i⟩, rfl⟩) f
+  rw [this fun i ↦ v (A (j, i)) * v (x i)]
+  refine ciSup_le fun i ↦ ?_
+  gcongr
+  · exact v.iSup_abv_nonneg
+  · exact Finite.le_ciSup_of_le (j, i) le_rfl
+  · exact Finite.le_ciSup_of_le i le_rfl
+
+variable [AdmissibleAbsValues K]
+
+open AdmissibleAbsValues
+
+open Multiset in
+/-- Let `A : ι' × ι → K`, which we can interpret as an `ι' × ι` matrix over `K`.
+Let `x : ι → K` be a tuple. Then the multiplicative height of `A x` is bounded by
+`#ι ^ totalWeight K * mulHeight A * mulHeight x`. -/
+theorem linearMap_apply_le [Nonempty ι] (A : ι' × ι → K) (x : ι → K) :
+    mulHeight (fun j ↦ ∑ i, A (j, i) * x i) ≤
+      Nat.card ι ^ totalWeight K * mulHeight A * mulHeight x := by
+  have H₀ : 1 ≤ Nat.card ι ^ totalWeight K * mulHeight A * mulHeight x := by
+    rw [show (1 : ℝ) = 1 * 1 * 1 by ring]
+    gcongr
+    · exact_mod_cast Nat.one_le_pow _ _ Nat.card_pos
+    · exact one_le_mulHeight _
+    · exact one_le_mulHeight _
+  rcases isEmpty_or_nonempty ι' with hι' | hι'
+  · simpa only [mulHeight_eq_one_of_isEmpty, mul_one] using H₀
+  rcases eq_or_ne (fun j ↦ ∑ i, A (j, i) * x i) 0 with h | h
+  · simpa only [h, mulHeight_zero] using H₀
+  rcases eq_or_ne A 0 with rfl | hA
+  · rwa [show (fun j ↦ ∑ i, (0 : ι' × ι → K) (j, i) * x i) = 0 by simp [Pi.zero_def],
+      mulHeight_zero]
+  rcases eq_or_ne x 0 with rfl | hx
+  · rwa [show (fun j ↦ ∑ i, A (j, i) * (0 : ι → K) i) = 0 by simp [Pi.zero_def], mulHeight_zero]
+  rw [mulHeight_eq h, mulHeight_eq hA, mulHeight_eq hx,
+    mul_mul_mul_comm, ← mul_assoc, ← mul_assoc, mul_assoc (_ * _ * _)]
+  gcongr
+  · exact finprod_nonneg fun v ↦ v.val.iSup_abv_nonneg
+  · refine mul_nonneg (mul_nonneg (by simp) ?_) ?_ <;>
+      exact prod_map_nonneg fun v _ ↦ v.iSup_abv_nonneg
+  · -- archimedean part: reduce to "local" statement `linearMap_apply_bound`
+    rw [mul_assoc, ← prod_map_mul, ← prod_replicate, totalWeight, ← map_const', ← prod_map_mul]
+    refine prod_map_le_prod_map₀ _ _ (fun v _ ↦ v.iSup_abv_nonneg) fun v _ ↦ ?_
+    rw [mul_comm (iSup _), ← mul_assoc]
+    exact linearMap_apply_bound v A x
+  · -- nonarchimedean part: reduce to "local" statement `linearMap_apply_bound_of_isNonarchimedean`
+    rw [← finprod_mul_distrib (mulSupport_iSup_nonarchAbsVal_finite hA)
+      (mulSupport_iSup_nonarchAbsVal_finite hx)]
+    refine finprod_le_finprod (mulSupport_iSup_nonarchAbsVal_finite h)
+      (fun v ↦ v.val.iSup_abv_nonneg) ?_ fun v ↦ ?_
+    · exact Function.finite_mulSupport_mul (mulSupport_iSup_nonarchAbsVal_finite hA)
+        (mulSupport_iSup_nonarchAbsVal_finite hx)
+    · exact linearMap_apply_bound_of_isNonarchimedean (isNonarchimedean _ v.prop) A x
+
+
+end Height
