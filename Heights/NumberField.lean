866cd80c4265af914668258d9ff87b3a7fd2c91e
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2026-02-28 19:39:27 +0100

    kill last sorry

--- Heights/NumberField.lean
+++ Heights/NumberField.lean
@@ -1,3 +1,4 @@
+import Mathlib.GroupTheory.FiniteAbelian.Basic
 import Mathlib.NumberTheory.Height.NumberField
 import Mathlib.RingTheory.UniqueFactorizationDomain.Finsupp
 import Heights.Basic
@@ -16,13 +17,67 @@ section API
 
 namespace Subgroup
 
-variable {G : Type*} [Group G] (H : Subgroup G) {K : Subgroup G} [H.Normal]
-
 @[to_additive]
-lemma pow_relIndex_mem {g : G} (hg : g ∈ K) : g ^ H.relIndex K ∈ H :=
+lemma pow_relIndex_mem {G : Type*} [Group G] (H : Subgroup G) {K : Subgroup G} [H.Normal]
+    {g : G} (hg : g ∈ K) : g ^ H.relIndex K ∈ H :=
   pow_index_mem (H.subgroupOf K) ⟨g, hg⟩
 
+@[to_additive]
+lemma index_ne_zero_of_range_powMonoidHom_le {A : Type*} [CommGroup A] [Group.FG A]
+    (B : Subgroup A) {n : ℕ} (hn : n ≠ 0) (h : (powMonoidHom (α := A) n).range ≤ B) :
+    B.index ≠ 0 := by
+  refine index_ne_zero_iff_finite.mpr <| CommGroup.finite_of_fg_torsion _ ?_
+  simp only [Monoid.IsTorsion, isOfFinOrder_iff_pow_eq_one]
+  refine fun g ↦ ⟨n, Nat.zero_lt_of_ne_zero hn, ?_⟩
+  have H a : a ^ n ∈ B := by
+    rw [SetLike.le_def] at h
+    have ha : a ^ n ∈ (powMonoidHom n).range := by simp
+    exact h ha
+  suffices Quotient.out g ^ n ∈ B by
+    obtain ⟨b, hb₁, hb₂⟩ : ∃ b ∈ B, b = Quotient.out g ^ n := ⟨_, this, rfl⟩
+    apply_fun QuotientGroup.mk (s := B) at hb₂
+    rw [(QuotientGroup.eq_one_iff b).mpr hb₁] at hb₂
+    rw [hb₂, QuotientGroup.mk_pow, QuotientGroup.out_eq']
+  exact H _
+
+@[to_additive]
+lemma relIndex_ne_zero_of_range_powMonoidHom_le {A : Type*} [CommGroup A] (B C : Subgroup A)
+    (hB : B.FG) {n : ℕ} (hn : n ≠ 0) (h : B.map (powMonoidHom (α := A) n) ≤ C) :
+    C.relIndex B ≠ 0 := by
+  simp only [relIndex]
+  have : Group.FG B := (Group.fg_iff_subgroup_fg B).mpr hB
+  refine index_ne_zero_of_range_powMonoidHom_le (A := B) (C.subgroupOf B) hn ?_
+  rw [SetLike.le_def] at h ⊢
+  rintro ⟨b, hbmem⟩ hb
+  simp only [mem_map, powMonoidHom_apply, forall_exists_index, and_imp, forall_apply_eq_imp_iff₂,
+    MonoidHom.mem_range, Subtype.exists, SubmonoidClass.mk_pow, Subtype.mk.injEq,
+    exists_prop] at h hb
+  rw [mem_subgroupOf, show Subtype.val ⟨b, hbmem⟩ = b from rfl]
+  obtain ⟨a, hamem, rfl⟩ := hb
+  exact mem_carrier.mp (h a hamem)
+
 end Subgroup
+
+namespace Submodule
+
+variable {R K : Type*} [CommRing R] [CommRing K] [Algebra R K] [Module.Finite ℤ R]
+
+lemma relIndex_ne_zero_of_map_linearMapMulLeft_le (A B : Submodule R K) {n : ℕ} (hn : n ≠ 0)
+    (hfg : A.FG) (h : A.map (LinearMap.mulLeft R (n : K)) ≤ B) :
+    B.toAddSubgroup.relIndex A.toAddSubgroup ≠ 0 := by
+  have hA : A.toAddSubgroup.FG := by
+    suffices A.toAddSubgroup.toIntSubmodule.FG by
+      rwa [fg_iff_addSubgroup_fg, AddSubgroup.toIntSubmodule_toAddSubgroup] at this
+    rw [Submodule.toIntSubmodule_toAddSubgroup A]
+    have : Module.Finite R ↥(restrictScalars ℤ A) := by
+      rwa [← Module.Finite.iff_fg] at hfg
+    nth_rw 1 [← Module.Finite.iff_fg]
+    exact Module.Finite.trans R _
+  refine A.toAddSubgroup.relIndex_ne_zero_of_range_nsmulAddMonoidHom_le B.toAddSubgroup hA hn ?_
+  rw [SetLike.le_def] at h ⊢
+  simpa using h
+
+end Submodule
 
 namespace Finite
 
@@ -792,17 +847,48 @@ info: AddSubgroup.relIndex_pointwise_smul.{u_1, u_2} {G : Type u_1} {H : Type u_
 #guard_msgs in
 #check AddSubgroup.relIndex_pointwise_smul
 
-open Pointwise in
+lemma relIndex_ne_zero (x : K) :
+    (Submodule.span (𝓞 K) {(1 : K)}).toAddSubgroup.relIndex
+      (Submodule.span (𝓞 K) {1, x}).toAddSubgroup ≠ 0 := by
+  obtain ⟨m, r, hm, h⟩ : ∃ (m : ℕ) (r : 𝓞 K), m ≠ 0 ∧  m • x = r := by
+    obtain ⟨num, ⟨d, hd⟩, h⟩ :=
+      IsLocalization.exists_mk'_eq (Algebra.algebraMapSubmonoid (𝓞 K) (nonZeroDivisors ℤ)) x
+    rw [show
+        (d ∈ Algebra.algebraMapSubmonoid ..) = ∃ a ∈ ↑(nonZeroDivisors ℤ), (algebraMap ..) a = d
+        from rfl] at hd
+    choose a hamem ha using hd
+    rw [mem_nonZeroDivisors_iff_ne_zero] at hamem
+    refine ⟨a.natAbs, a.sign * num, Int.natAbs_ne_zero.mpr hamem, ?_⟩
+    have Ha : (a.natAbs : 𝓞 K) * num = (a.sign * num) * (a : 𝓞 K) := by
+      rw [mul_right_comm]
+      congr
+      rw [← Int.cast_mul, Int.sign_mul_self]
+      exact (Int.cast_natCast a.natAbs).symm
+    rw [← h, nsmul_eq_mul,
+      show (a.natAbs : K) * _ = (a.natAbs : 𝓞 K) • _ from (smul_eq_mul ..).symm,
+      IsLocalization.smul_mk', Ha, ← IsLocalization.smul_mk']
+    simp only [← ha, algebraMap_int_eq, eq_intCast, IsLocalization.mk'_self, map_mul, map_intCast]
+    rw [← smul_eq_mul (↑a.sign) ((algebraMap (𝓞 K) K) num), Algebra.smul_def, mul_one]
+    rfl
+  refine Submodule.relIndex_ne_zero_of_map_linearMapMulLeft_le _ _ hm (Submodule.fg_span (by simp)) ?_
+  rw [LinearMap.map_span_le]
+  intro a ha
+  simp only [Set.mem_insert_iff, Set.mem_singleton_iff] at ha
+  rw [LinearMap.mulLeft_apply, Submodule.mem_span_singleton]
+  rcases ha with rfl | rfl
+  · refine ⟨m, ?_⟩
+    rw [← Algebra.algebraMap_eq_smul_one, mul_one]
+    norm_cast
+  · refine ⟨r, ?_⟩
+    rw [← Algebra.algebraMap_eq_smul_one, ← RingOfIntegers.coe_eq_algebraMap, ← h, nsmul_eq_mul]
+
 lemma exists_nat_ne_zero_exists_integer_mul_eq_and_absNorm_span_eq_pow (x : K) :
     ∃ n : ℕ, n ≠ 0 ∧ ∃ a : 𝓞 K, n * x = a ∧
       (Ideal.span {(n : 𝓞 K), a}).absNorm = n ^ (Module.finrank ℚ K - 1) := by
-  -- Maybe state this right away for `n = absNorm D`, where `D` is the "denominator ideal"
-  -- of `x` (which, however, is not yet defined in Mathlib).
-  -- An alternative definition of `n` is as the index of `𝓞 K` in `𝓞 K + 𝓞 K • x`.
   let R : Submodule (𝓞 K) K := Submodule.span (𝓞 K) {1}
   let I : Submodule (𝓞 K) K := Submodule.span (𝓞 K) {1, x}
   let n := R.toAddSubgroup.relIndex I.toAddSubgroup
-  have hn : n ≠ 0 := sorry
+  have hn : n ≠ 0 := relIndex_ne_zero x
   have ha₁ : n * x ∈ R := by
     rw [← nsmul_eq_mul]
     have hx : x ∈ I := Submodule.mem_span_of_mem <| Set.mem_insert_of_mem 1 rfl
